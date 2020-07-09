235c9b5d76849ae1fbba3ca2978f98f4daee61b5
Harrys Moba <harrys-12@hotmail.com>
2020-07-09 16:21:32 -0400

    Sección 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +21,12 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
