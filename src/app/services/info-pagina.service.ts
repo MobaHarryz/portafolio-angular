30019b6c2ff6cea4fc437c47b72f8f6e8cfd88dd
Harrys Moba <harrys-12@hotmail.com>
2020-07-13 16:04:40 -0400

    Fin Sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,26 +10,38 @@ export class InfoPaginaService {
   info: InfoPagina = {}; 
   cargada = false;
 
+  equipo: any [] = [];
+
   constructor( private http: HttpClient ) {
 
     //console.log('info de Servicio de Pagina listo');   
-    
-    //Leer un archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-          .subscribe( (resp: InfoPagina) =>{
-           
 
-            this.cargada = true;
-            this.info = resp;
-
-            console.log(resp); //se puede elegir la respuesta que quiero mostrar ya que es un objeto.
-
-          })
-
+    this.cargarInfo();
+    this.cargarEquipo();
 
 
    }
 
+    private cargarInfo (){
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) =>{
+       
+        this.cargada = true;
+        this.info = resp;
+        //se puede elegir la respuesta que quiero mostrar ya que es un objeto.
+
+      })
+    }
 
    
+    private cargarEquipo (){
+      this.http.get('https://angular-html-9b023.firebaseio.com/equipo.json')
+      .subscribe( (resp: any []) =>{
+       
+        this.equipo = resp;
+        console.log(resp); //se puede elegir la respuesta que quiero mostrar ya que es un objeto.
+
+      })
+    }
+
 }
